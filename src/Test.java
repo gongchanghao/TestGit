5238e1226c81e6d78d8daf8782a31e5d3a2d9172
gongchanghao <gongchanghao@qq.com>
2018-10-12 10:32:45 +0800

    测试一下用户名和邮箱是否改变

--- src/Test.java
+++ src/Test.java
@@ -2,9 +2,14 @@ public class Test
 {
     public static void main(String[] args) {
         System.out.println(getTitle());
+        System.out.println(getDesc());
     }
 
     public static String getTitle(){
         return "这是在测试git的分支。。。。";
+    }
+
+    public static String getDesc(){
+        return "这是在测试git的用户名和邮箱有没有改变";
     }
 }
